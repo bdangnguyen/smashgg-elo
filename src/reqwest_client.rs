ad936a54258e23421cc65dcb3116ac53cae934f8
bdangnguyen <bryandnguyen2120@gmail.com>
2022-03-29 16:43:14 -0700

    Format and update the wrapper

--- src/reqwest_client.rs
+++ src/reqwest_client.rs
@@ -1,19 +1,20 @@
-
-use std::collections::HashMap;
 use reqwest::blocking::Client;
 use reqwest::blocking::Response;
 use reqwest::header::HeaderMap;
 use serde_json::Value;
+use std::collections::HashMap;
+
+const SMASH_URL: &str = "https://api.smash.gg/gql/alpha";
 
 pub struct ReqwestClient {
-    client: Client
+    client: Client,
 }
 
 impl Default for ReqwestClient {
     fn default() -> Self {
-        ReqwestClient { 
+        ReqwestClient {
             client: reqwest::blocking::Client::new(),
-         }
+        }
     }
 }
 
@@ -22,11 +23,14 @@ impl ReqwestClient {
         ReqwestClient::default()
     }
 
-    pub fn send_post(self, headers: HeaderMap, json_content: &HashMap<&str, Value>) -> Response {
-        let result = match self.client.post(smashgg_elo_rust::SMASH_URL)
-        .headers(headers.clone())
-        .json(&json_content)
-        .send() {
+    pub fn send_post(&self, headers: HeaderMap, json_content: &HashMap<&str, Value>) -> Response {
+        let result = match self
+            .client
+            .post(SMASH_URL)
+            .headers(headers)
+            .json(&json_content)
+            .send()
+        {
             Ok(response) => response,
             Err(err) => panic!("Error in sending post request: {}", err),
         };
